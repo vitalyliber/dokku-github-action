66101db03eebaa4c93d30cebe2aff9abcc4d5197
Vitaly Liber <zenamax@gmail.com>
2019-06-10 16:17:30 +0300

    fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,11 +5,18 @@ if [ -z "$SSH_PRIVATE_KEY" ]; then
 	exit 1
 fi
 
+if [ -z "$SSH_PUBLIC_KEY" ]; then
+	>&2 echo "Set SSH_PUBLIC_KEY environment variable"
+	exit 1
+fi
+
 echo "Private key $SSH_PRIVATE_KEY"
 
 mkdir -p ~/.ssh
 echo "$SSH_PRIVATE_KEY" | tr -d '\r' > ~/.ssh/id_rsa
+echo "$SSH_PUBLIC_KEY" | tr -d '\r' > ~/.ssh/id_rsa.pub
 chmod 600 ~/.ssh/id_rsa
+chmod 600 ~/.ssh/id_rsa.pub
 ssh-keyscan -t rsa "$SSH_HOST" >> ~/.ssh/known_hosts
 
 sh -c "$*"
