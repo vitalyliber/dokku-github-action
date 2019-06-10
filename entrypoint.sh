082ee2fd9101aea124386b242a1e189e6dd8072f
Vitaly Liber <zenamax@gmail.com>
2019-06-10 14:45:11 +0300

    fixed

--- entrypoint.sh
+++ entrypoint.sh
@@ -10,6 +10,6 @@ echo "Private key $SSH_PRIVATE_KEY"
 mkdir -p ~/.ssh
 echo "$SSH_PRIVATE_KEY" | tr -d '\r' > ~/.ssh/id_rsa
 chmod 600 ~/.ssh/id_rsa
-ssh -o StrictHostKeyChecking=no $SSH_HOST uptime
+ssh-keyscan -H "$SSH_HOST" >> ~/.ssh/known_hosts
 
 sh -c "$*"
