21f00e5138f5fca628a3ebd29c03518dd640adfa
Vitaly Liber <zenamax@gmail.com>
2019-08-13 23:01:21 +0300

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,27 +1,23 @@
 #!/bin/sh
 
-# set -e
+set -e
 
-# SSH_PATH="$HOME/.ssh"
+SSH_PATH="$HOME/.ssh"
 
-# mkdir -p "$SSH_PATH"
-# touch "$SSH_PATH/known_hosts"
+mkdir -p "$SSH_PATH"
+touch "$SSH_PATH/known_hosts"
 
-# echo "$PRIVATE_KEY" > "$SSH_PATH/deploy_key"
-# echo "$PUBLIC_KEY" > "$SSH_PATH/deploy_key.pub"
+echo "$PRIVATE_KEY" > "$SSH_PATH/deploy_key"
+echo "$PUBLIC_KEY" > "$SSH_PATH/deploy_key.pub"
 
-# chmod 700 "$SSH_PATH"
-# chmod 600 "$SSH_PATH/known_hosts"
-# chmod 600 "$SSH_PATH/deploy_key"
-# chmod 600 "$SSH_PATH/deploy_key.pub"
+chmod 700 "$SSH_PATH"
+chmod 600 "$SSH_PATH/known_hosts"
+chmod 600 "$SSH_PATH/deploy_key"
+chmod 600 "$SSH_PATH/deploy_key.pub"
 
-# eval $(ssh-agent)
-# ssh-add "$SSH_PATH/deploy_key"
+eval $(ssh-agent)
+ssh-add "$SSH_PATH/deploy_key"
 
-# ssh-keyscan -t rsa $HOST >> "$SSH_PATH/known_hosts"
-
-ls -la
-cd ..
-ls -la
+ssh-keyscan -t rsa $HOST >> "$SSH_PATH/known_hosts"
 
 GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" git push dokku@$HOST:$PROJECT master
