9ef6570372de032df226f65498e8e963bfe00ec8
William Mayor <mail@mayortech.co.uk>
2020-05-16 13:22:02 +0100

    Add debug log lines

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,23 +1,28 @@
 #!/bin/sh
 set -e
 
+echo "Setting up SSH directory"
 SSH_PATH="$HOME/.ssh"
 mkdir -p "$SSH_PATH"
 chmod 700 "$SSH_PATH"
 
+echo "Saving SSH key"
 echo "$PRIVATE_KEY" > "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key"
 
 if [ -n "$FORCE_DEPLOY" ]; then
+    echo "Enabling force deploy"
     FORCE_DEPLOY="--force"
 fi
 
 GIT_SSH_COMMAND="ssh -p ${PORT-22} -i $SSH_PATH/deploy_key"
 
 if [ -n "$HOST_KEY" ]; then
+    echo "Adding hosts key to known_hosts"
     echo "$HOST_KEY" >> "$SSH_PATH/known_hosts"
     chmod 600 "$SSH_PATH/known_hosts"
 else
+    echo "Disabling host key checking"
     GIT_SSH_COMMAND="$GIT_SSH_COMMAND -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
 fi
 
