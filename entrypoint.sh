44e40462af05a45322ba0b5c6950ad73afeb23d1
William Mayor <mail@mayortech.co.uk>
2020-05-16 12:24:08 +0100

    Add way to run action with/without a host key

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,28 +3,34 @@
 set -e
 
 SSH_PATH="$HOME/.ssh"
-DEPLOY_BRANCH="${BRANCH-master}"
-
-FORCE=$([ "$FORCE_DEPLOY" = true ] && echo "--force" || echo "")
-
 mkdir -p "$SSH_PATH"
-touch "$SSH_PATH/known_hosts"
+
+DEPLOY_BRANCH="${BRANCH-master}"
+FORCE=$([ "$FORCE_DEPLOY" = true ] && echo "--force" || echo "")
 
 echo "$PRIVATE_KEY" > "$SSH_PATH/deploy_key"
 echo "$PUBLIC_KEY" > "$SSH_PATH/deploy_key.pub"
 
 chmod 700 "$SSH_PATH"
-chmod 600 "$SSH_PATH/known_hosts"
 chmod 600 "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key.pub"
 
 eval $(ssh-agent)
 ssh-add "$SSH_PATH/deploy_key"
 
-ssh-keyscan -t rsa $HOST >> "$SSH_PATH/known_hosts"
+
+GIT_SSH_COMMAND="ssh -p ${PORT-22}"
+
+
+if [ -n "$HOST_KEY" ]; then
+    echo "$HOST_KEY" >> "$SSH_PATH/known_hosts"
+    chmod 600 "$SSH_PATH/known_hosts"
+else
+    GIT_SSH_COMMAND="$GIT_SSH_COMMAND -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no"
+fi
 
 git checkout $DEPLOY_BRANCH
 
 echo "The deploy is starting"
 
-GIT_SSH_COMMAND="ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -p ${PORT-22}" git push dokku@$HOST:$PROJECT $DEPLOY_BRANCH:master $FORCE
+GIT_SSH_COMMAND="$GIT_SSH_COMMAND" git push dokku@$HOST:$PROJECT $DEPLOY_BRANCH:master $FORCE
---
+++ tests/host_key_fails.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+docker run \
+    --env "BRANCH=$BRANCH" \
+    --env "PRIVATE_KEY=$PRIVATE_KEY" \
+    --env "PUBLIC_KEY=$PUBLIC_KEY" \
+    --env "HOST=$HOST" \
+    --env "HOST_KEY=123" \
+    --env "PROJECT=$PROJECT" \
+    --workdir "/repo" \
+    --volume "$LOCAL_REPO:/repo" \
+    dokku-github-action
