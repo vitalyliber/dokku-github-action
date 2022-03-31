e54c7d1d83ec4a6cc9deb3b7db7a75ad019095a2
vitaly.liber <zenamax@gmail.com>
2022-03-31 11:21:37 +0300

    Deploy from a non-master branch

--- entrypoint.sh
+++ entrypoint.sh
@@ -13,7 +13,8 @@ chmod 600 "$SSH_PATH/deploy_key"
 GIT_COMMAND="git push dokku@$HOST:$PROJECT"
 
 echo "Detect the project default branch: master or main"
-DEFAULT_BRANCH="$(git branch -l master main | sed 's/^* //' | sed 's/ ^* //')"
+DEFAULT_BRANCH="$(git remote show origin | awk '/HEAD branch/ {print $NF}')"
+echo "Default is $DEFAULT_BRANCH"
 
 if [ -n "$BRANCH" ]; then
     GIT_COMMAND="$GIT_COMMAND $BRANCH:$DEFAULT_BRANCH"
